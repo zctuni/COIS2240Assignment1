13056d04346414e6fd1d15a6531cc096d30d5054
zctuni <zacharycox@trentu.ca>
2018-02-04 18:30:12 -0500

    iterative calculation implementation

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Welcome the user and introduce the program.
+		// Welcome the user and introduce the program.
 		System.out.println("This is a Fibonacci number generator.");
 		
-		//Ask user how many Fibonacci numbers to calculate.
+		// Ask user how many Fibonacci numbers to calculate.
 		System.out.println("How many Fibonacci numbers would you like to generate?");
 		
 		Scanner scanner = new Scanner(System.in);
@@ -15,10 +15,20 @@ public class Main {
 		
 		
 		// Display recursive Fibonacci results
-		System.out.println("Recursive Fibonacci Number Generation:"); //Inform user of calculation method
+		System.out.println("Recursive Fibonacci Number Generation:"); // Inform user of calculation method
 		
 		for (int i = 0; i < input; i++) {
 			System.out.println(fiboSeriesRec(i));
+			
+		}
+		
+		System.out.println(" "); // Output blank line to console for formatting reasons
+		
+		// Display iterative Fibonacci results
+		System.out.println("Iterative Fibonacci Number Generation:"); // Inform user of calculation method		
+		
+		for (int i = 0; i < input; i++) {
+			System.out.println(fiboSeriesIte(i));	
 			
 		}
 	}
@@ -32,6 +42,21 @@ public class Main {
 		return fiboSeriesRec(number-1) + fiboSeriesRec(number-2);
 	}
 	
+	
+	// Iterative Fibonacci number calculation
+	public static int fiboSeriesIte(int number) {
+		int fib1 = 0;
+		int fib2 = 1;
+		int fibValue =0;
+		
+		for (int count = 0; count <= number; count++) {
+			fibValue = fib1;
+			fib1 = fib1 + fib2;
+			fib2 = fibValue;
+		}
+		
+		return fibValue;
+	}
 	
 	
 }
