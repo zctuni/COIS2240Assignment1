67bf1fb46dae63ab8e6c67e03c079c760d3f6112
zctuni <zacharycox@trentu.ca>
2018-02-04 17:34:03 -0500

    initial user welcome and request for input

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Welcome the user and introduce the program.
+		System.out.println("This is a Fibonacci number generator.");
+		
+		//Ask user how many Fibonacci numbers to calculate.
+		System.out.println("How many Fibonacci numbers would you like to generate?");
+		
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+	
+	}
+	
+	
+}
